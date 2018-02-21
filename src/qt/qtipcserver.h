57e240d9cf1bb286a6abe49e7671ec4f7d14728a
technolog <viktor@allz.su>
2018-02-21 22:14:41 +0300

    qt

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define PostCoin-qt message queue name
+#define BITCOINURI_QUEUE_NAME "PostCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
